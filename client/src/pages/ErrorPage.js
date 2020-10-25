81ba1e880ca6df0350f120e0939b9f2b2cabcb51
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-10-26 00:55:35 +0200

    Created Error page

---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ErrorComponent from "../components/ErrorComponent/ErrorComponent";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <ErrorComponent />
+    </div>
+  );
+};
+
+export default ErrorPage;
