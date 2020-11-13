044ce9273ae622c3baab92acd713430bc13f3d96
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-13 18:51:17 +0200

    Error 404 Page Not Found Component

---
+++ client/src/components/layout/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const ErrorComponent = () => {
+  return (
+    <div className='PageNotFound'>
+      <div className='PageNotFound__Error'>
+        <h1>Error 404</h1>
+        <div>
+          <NavLink to='/'>
+            <h1>GO BACK HOME</h1>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorComponent;
