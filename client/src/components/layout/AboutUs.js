e1f9b8c5e5e1e098688e248bca535302f279b4e3
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-13 18:51:39 +0200

    About Us Info Component

---
+++ client/src/components/layout/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EliasSVG from '../SVGComponents/EliasSVG';
+
+const AboutUsComponent = () => {
+  return (
+    <div>
+      <div>About Us Page</div>
+      <EliasSVG />
+    </div>
+  );
+};
+
+export default AboutUsComponent;
