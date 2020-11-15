c96bf94fdf318ff7fe43a4e933aadc7cd838164d
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-15 14:53:20 +0200

    Login form setup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  // Destructing
+  const { email, password } = formData;
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value, // This way we can handleChange on every field
+    });
+  };
+  const handleSubmit = () => {};
   return (
     <>
-      <div>
-        <h4>Login Page</h4>
-      </div>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign into your Account
+      </p>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => handleChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
     </>
   );
 };
