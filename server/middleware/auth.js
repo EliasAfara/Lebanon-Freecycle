c05bd940b27390d9d765e9574e46a9eff6cb6ddb
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-15 17:52:04 +0200

    :passport_control: Costume Middleware for Authentication

---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+// Costume Middleware
+
+export default function auth(req, res, next) {
+  // Get the token from the header
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    // 401 not authorized
+    return res.status(401).json({ msg: 'No token, autorization denied' });
+  }
+
+  // Verify token
+  try {
+    // Decoed the token
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    // Take req object and assign a value to user
+    req.user = decoded.user; // has user with id in the payload
+    next();
+  } catch (err) {
+    // Run if token is not valid
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+}
