fb585bc33e03229ae0abc36b5b4061b9aef79236
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-15 17:55:00 +0200

    :sparkles: Login, Registratoin & Authentication Routes Setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import connectDB from './config/db.js';
 
+// Routes
+import registerRoute from './routes/register.js';
+import loginRoute from './routes/login.js';
+import authRoute from './routes/auth.js';
+
 const app = express();
 
 // Connect Database
@@ -9,7 +14,12 @@ connectDB();
 // Init Middleware (Allow us to get the data from frontend)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+//app.get('/', (req, res) => res.send('API RUNNING'));
+
+// Define Routes
+app.use('/api/register', registerRoute);
+app.use('/api/login', loginRoute);
+app.use('/api/auth', authRoute);
 
 const PORT = process.env.PORT || 5000;
 
