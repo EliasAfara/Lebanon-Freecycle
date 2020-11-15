0acfd03b5f935e86959ea2254d8a2a142d52b261
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-15 15:28:01 +0200

    :wrench: Established connection with MongoDB

---
+++ server/config/db.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+      console.log('MongoDB Connected...')
+    );
+  } catch (err) {
+    console.error(err.message);
+
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+export default connectDB;
