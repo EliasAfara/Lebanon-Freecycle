f00e524cda6a3416debd794185ac3d45882d2bc2
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-17 03:48:07 +0200

    Login Form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
-const Login = () => {
+const Login = ({ loginUser, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -16,7 +19,16 @@ const Login = () => {
       [e.target.name]: e.target.value, // This way we can handleChange on every field
     });
   };
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Login function');
+    loginUser(email, password);
+  };
+
+  // Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to='/donations' />;
+  }
   return (
     <>
       <h1 className='large text-primary'>Sign In</h1>
@@ -54,4 +66,13 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
