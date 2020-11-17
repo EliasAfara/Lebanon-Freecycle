dc68fa356f1a16da7d553d0186e9bcf4f8c74ebf
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-17 03:43:15 +0200

    Actions Types

---
+++ client/src/actions/types.js
@@ -0,0 +1,16 @@
+// Alert types
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+// Registration types
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
+// Authentication types
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+// Login types
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
