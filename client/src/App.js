f572a7dc7c86b9b209c4b34a633e69c82f72a761
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-17 03:50:03 +0200

    Front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+// Components
 import Navbar from './components/NavbarComponent';
 import UserNavbar from './components/UserNavbarComponent';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
+// Utils
 import PageShell from './utils/PageShell';
 import ScrollToTop from './utils/scrollToTop';
+import setAuthToken from './utils/setAuthToken';
+
+// Routes
 import Routes from './components/routing/Routes';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+
+// Check if token avaiable
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   const isLoggedIn = false;
   return (
-    <>
+    <Provider store={store}>
       <Router>
         <ScrollToTop />
 
@@ -23,7 +44,7 @@ function App() {
           <Route component={Routes} />
         </Switch>
       </Router>
-    </>
+    </Provider>
   );
 }
 
