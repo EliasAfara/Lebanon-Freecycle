d801ff00a28d343d2af74f7240371ffcf16d26d1
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-17 16:06:27 +0200

    Clean up

--- client/src/components/UserNavbarComponent/index.js
+++
@@ -1,9 +0,0 @@
-// Hear will import both user Navbars and will create a condition that will change the navbar when the width changes
-
-import React from "react";
-
-const UserNavbar = () => {
-  return <>User Navbar</>;
-};
-
-export default UserNavbar;
