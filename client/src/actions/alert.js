8dd73655fa90f0a4a72f989fe4d4e96e3b5eeb9c
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-18 19:08:38 +0200

    Increased alert duration to 5 seconds

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -10,5 +10,5 @@ export const setAlert = (msg, alertType) => (dispatch) => {
     payload: { msg, alertType, id },
   });
 
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 3000);
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
 };
