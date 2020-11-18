d6e088b8a745ab5da754a4dc218e804a2121bd04
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-18 19:10:13 +0200

    Added username Key

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {
+  fullname: {
     type: String,
     required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
   email: {
     type: String,
