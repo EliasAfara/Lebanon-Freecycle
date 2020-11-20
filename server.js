94e5b9abd5d564a6497daba5c4ee3eccf8f8cb2f
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-20 02:11:07 +0200

    Added XXS Clean

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const xss = require('xss-clean');
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Init Middleware (Allow us to get the data from frontend)
+app.use(express.json({ extended: false }));
+
+// Prevent XSS attacks
+app.use(xss());
+
+app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Routes
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
 const authRoute = require('./routes/auth');
 
-const app = express();
-
-// Connect Database
-connectDB();
-
-// Init Middleware (Allow us to get the data from frontend)
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('API RUNNING'));
-
-// Define Routes
+// Mount Routes
 app.use('/api/register', registerRoute);
 app.use('/api/login', loginRoute);
 app.use('/api/auth', authRoute);
