838e4f2d808819cce645471c44683ea09729c1f1
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-26 23:50:35 +0200

    Installed Ant Design

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import 'antd/dist/antd.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
