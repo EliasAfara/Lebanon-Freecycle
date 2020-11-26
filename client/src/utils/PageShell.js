94046fe6fea4f8d6b5bdce0ffffc942787d63400
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-26 23:49:32 +0200

    Cancled Page Shell because of a warning

--- client/src/utils/PageShell.js
+++ client/src/utils/PageShell.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 //import { TransitionGroup } from "react-transition-group";
+
+// NEED TO BE FIXED CUZ ITS GIVING A WARNING
+
 const PageShell = (Page) => {
   return (props) => (
-    <div className="page">
+    <div className='page'>
       <ReactCSSTransitionGroup
         transitionAppear={true}
         transitionAppearTimeout={400}
         transitionEnterTimeout={400}
         transitionLeaveTimeout={200}
-        transitionName={"SlideIn"}
+        transitionName={'SlideIn'}
       >
         <Page {...props} />
       </ReactCSSTransitionGroup>
