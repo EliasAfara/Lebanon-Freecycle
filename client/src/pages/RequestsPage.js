9a26d50f41f4bf0c654a7f3c3e0c264dda5942a8
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-27 22:31:45 +0200

    Requests Page Display

--- client/src/pages/RequestsPage.js
+++ client/src/pages/RequestsPage.js
@@ -1,13 +1,109 @@
 import React from 'react';
+//import PropTypes from 'prop-types'
 
-const RequestsPage = () => {
+import ItemCard from '../components/ItemCard/ItemCard';
+import Tabs from '../components/Tabs/NavTabs';
+
+const ItemDetails = {
+  UserAvatar: 'https://semantic-ui.com/images/avatar2/small/mark.png',
+  FullName: 'Elias Afara',
+  Username: 'elias',
+  ItemName: 'Couch',
+  ItemCategory: 'Fourniture',
+  ItemState: 'Used',
+  ItemLocation: 'Tyre, Lebanon',
+  ItemAddress: 'Tyre, Behind Al Ekhlas Resturant',
+  ItemDescription: 'Old and used couch',
+  ItemDateOfCreation: '11/26/2020 11:57 AM',
+  ItemID: '123456789',
+  status: true,
+};
+
+const {
+  UserAvatar,
+  FullName,
+  Username,
+  ItemName,
+  ItemCategory,
+  ItemState,
+  ItemLocation,
+  ItemAddress,
+  ItemDescription,
+  ItemDateOfCreation,
+  ItemID,
+  status,
+} = ItemDetails;
+
+const AvailableRequests = (
+  // Should be an array that contains all the available Requests
+  // maps over the Requests schema
+  <ItemCard
+    UserAvatar={UserAvatar}
+    FullName={FullName}
+    Username={Username}
+    ItemName={ItemName}
+    ItemCategory={ItemCategory}
+    ItemState={ItemState}
+    ItemLocation={ItemLocation}
+    ItemAddress={ItemAddress}
+    ItemDescription={ItemDescription}
+    ItemDateOfCreation={ItemDateOfCreation}
+    ItemID={ItemID}
+    ItemStatus={status}
+  />
+);
+
+const CompletedRequests = (
+  // Should be an array that contains all the completed Requests
+  // maps over the Requests schema
+  <>
+    <ItemCard
+      UserAvatar={UserAvatar}
+      FullName={FullName}
+      Username={Username}
+      ItemName={ItemName}
+      ItemCategory={ItemCategory}
+      ItemState={ItemState}
+      ItemLocation={ItemLocation}
+      ItemAddress={ItemAddress}
+      ItemDescription={ItemDescription}
+      ItemDateOfCreation={ItemDateOfCreation}
+      ItemID={ItemID}
+      ItemStatus={false}
+    />
+    <ItemCard
+      UserAvatar={UserAvatar}
+      FullName={FullName}
+      Username={Username}
+      ItemName={ItemName}
+      ItemCategory={ItemCategory}
+      ItemState={ItemState}
+      ItemLocation={ItemLocation}
+      ItemAddress={ItemAddress}
+      ItemDescription={ItemDescription}
+      ItemDateOfCreation={ItemDateOfCreation}
+      ItemID={ItemID}
+      ItemStatus={false}
+    />
+  </>
+);
+
+const RequestsPage = (props) => {
   return (
     <>
-      <div>
-        <h4>Requests Page</h4>
-      </div>
+      <Tabs
+        firstTab='Available'
+        secondTab='Completed'
+        firstComponent={AvailableRequests}
+        secondComponent={CompletedRequests}
+        size='large'
+      />
     </>
   );
 };
+
+// Requests.propTypes = {
+
+// }
 
 export default RequestsPage;
