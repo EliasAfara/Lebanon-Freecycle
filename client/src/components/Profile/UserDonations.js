446747ffa2f99958c658888adb0801c7876e51cb
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-11-29 22:45:13 +0200

    Modifed Profile Bottom Style

--- client/src/components/Profile/UserDonations.js
+++ client/src/components/Profile/UserDonations.js
@@ -3,12 +3,19 @@ import Tabs from '../Tabs/NavTabs';
 
 const UserDonations = ({ AvailableDonations, CompletedDonations }) => {
   return (
-    <Tabs
-      firstTab='Available'
-      secondTab='Completed'
-      firstComponent={AvailableDonations}
-      secondComponent={CompletedDonations}
-    />
+    <div
+      style={{
+        borderRadius: '3px',
+        border: '1px solid #f0f0f0',
+      }}
+    >
+      <Tabs
+        firstTab='Available'
+        secondTab='Completed'
+        firstComponent={AvailableDonations}
+        secondComponent={CompletedDonations}
+      />
+    </div>
   );
 };
 
--- client/src/components/Profile/UserRequests.js
+++ client/src/components/Profile/UserRequests.js
@@ -3,12 +3,19 @@ import Tabs from '../Tabs/NavTabs';
 
 const UserRequests = ({ AvailableRequests, CompletedRequests }) => {
   return (
-    <Tabs
-      firstTab='Available'
-      secondTab='Completed'
-      firstComponent={AvailableRequests}
-      secondComponent={CompletedRequests}
-    />
+    <div
+      style={{
+        borderRadius: '3px',
+        border: '1px solid #f0f0f0',
+      }}
+    >
+      <Tabs
+        firstTab='Available'
+        secondTab='Completed'
+        firstComponent={AvailableRequests}
+        secondComponent={CompletedRequests}
+      />
+    </div>
   );
 };
 
