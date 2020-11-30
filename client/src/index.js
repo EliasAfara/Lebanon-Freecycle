b873dd9fbb57dc45a6b4a8664d04456f4ab7cb6d
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-01 00:03:53 +0200

    Added react phone input

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import 'antd/dist/antd.css';
+import 'react-phone-input-2/lib/style.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
