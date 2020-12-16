d65a4a32a664a02b1e66cfdfdfebd581dd3265f1
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-17 01:30:43 +0200

    Filter Bar Initial Draft

---
+++ client/src/components/FilterBar/FilterBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './FilterBar.css';
+
+const FilterBar = () => {
+  return (
+    <>
+      {/* <div className='barContainer'>
+      <div>
+        <select defaultValue={'DEFAULT'}>
+          <option value='DEFAULT' disabled hidden>
+            Date
+          </option>
+          <option value='Recent'>Recent</option>
+          <option value='Old'>Old</option>
+        </select>
+      </div>
+      <div>
+        <select defaultValue={'DEFAULT'}>
+          <option value='DEFAULT' disabled hidden>
+            Status
+          </option>
+          <option value='All'>All</option>
+          <option value='Available'>Available</option>
+          <option value='Completed'>Completed</option>
+        </select>
+      </div>
+      <div>
+        <select defaultValue={'DEFAULT'}>
+          <option value='DEFAULT' disabled hidden>
+            Location
+          </option>
+          <option value=''>Tyre</option>
+          <option value=''>Beirut</option>
+        </select>
+      </div>
+    </div> */}
+    </>
+  );
+};
+
+export default FilterBar;
