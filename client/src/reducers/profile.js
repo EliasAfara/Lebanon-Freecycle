1b367e2d8c5d5b8c776908d1e76809c6c4aa5b8c
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-18 23:56:41 +0200

    💥 Profile Reducer

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,35 @@
+import {
+  GET_USER_PROFILE,
+  UPDATE_USER_PROFILE,
+  USER_PROFILE_ERROR,
+} from '../actions/types';
+
+const initialState = {
+  profile: null,
+  loading: true,
+  error: {},
+};
+
+export default function profile(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_USER_PROFILE:
+    case UPDATE_USER_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case USER_PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+        profile: null,
+      };
+
+    default:
+      return state;
+  }
+}
