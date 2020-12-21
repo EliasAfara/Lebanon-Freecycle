f947ad7e41a66fdea94f695589259d5e129e5dc7
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-21 23:46:29 +0200

    ✨ Theme Inital Commit

--- client/src/App.js
+++ client/src/App.js
@@ -20,22 +20,60 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 
+// import { ThemeProvider } from 'styled-components';
+// import { GlobalStyles } from './components/StyledComponents/globalStyles';
+// import { lightTheme, darkTheme } from './components/StyledComponents/Themes';
+
 // Check if token avaiable
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
+// function getInitialTheme() {
+//   const savedTheme = localStorage.getItem('theme');
+//   return savedTheme ? savedTheme : 'light';
+// }
+
+// function getInitialThemeStyles() {
+//   const savedTheme = localStorage.getItem('theme');
+//   return savedTheme === 'dark' ? darkTheme : lightTheme;
+// }
+
 function App() {
+  // const [theme, setTheme] = useState(getInitialTheme);
+  // const [storedTheme, setStoredTheme] = useState(getInitialThemeStyles);
+
+  // useEffect(() => {
+  //   localStorage.setItem('theme', theme);
+  //   const currentTheme = localStorage.getItem('theme');
+  //   if (currentTheme === theme && theme === 'light') {
+  //     setStoredTheme(lightTheme);
+  //   } else if (theme === 'dark') {
+  //     setStoredTheme(darkTheme);
+  //   }
+  // }, [theme]);
+
+  // const themeToggler = () => {
+  //   theme === 'light' ? setTheme('dark') : setTheme('light');
+  // };
+
   useEffect(() => {
     store.dispatch(loadUser());
   }, []);
 
   return (
+    // <ThemeProvider theme={storedTheme}>
+    //   <>
+    //     <GlobalStyles />
+
+    //   </>
+    // </ThemeProvider>
     <Provider store={store}>
       <Router>
         <ScrollToTop />
 
         <Navbar />
+        {/* <button onClick={themeToggler}>Switch Theme</button> */}
 
         <Switch>
           <Route exact path='/' component={HomePage} />
---
+++ client/src/components/StyledComponents/Themes.js
@@ -0,0 +1,27 @@
+export const lightTheme = {
+  body: '#FAFAFA',
+  headerBg: '#FFFFFF',
+  text: '#363537',
+  toggleBorder: '#FFF',
+  cardBg: '#fafffa',
+};
+export const darkTheme = {
+  body: '#222831',
+  headerBg: 'linear-gradient(to right, #302E24, #28313E)',
+  text: '#F9FFEE',
+  toggleBorder: '#6B8096',
+  cardBg: '#1A1D24',
+};
+
+// export const solarizedDarkTheme = {
+//   body: '#FFF',
+//   text: '#363537',
+//   toggleBorder: '#FFF',
+//   background: '#363537',
+// };
+// export const solarizedTheme = {
+//   body: '#363537',
+//   text: '#FAFAFA',
+//   toggleBorder: '#6B8096',
+//   background: '#999',
+// };
