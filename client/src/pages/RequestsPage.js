55a5a0ef3803383409236846a4ff17f2892fc34a
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-26 22:26:49 +0200

    ⚡️ Request Page with FILTER & PAGINATION

--- client/src/pages/RequestsPage.js
+++ client/src/pages/RequestsPage.js
@@ -1,105 +1,151 @@
-import React from 'react';
-//import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+// Redux
+import { connect } from 'react-redux';
+import { getAllRequests } from '../actions/requests';
 
 import ItemCard from '../components/ItemCard/ItemCard';
-import FilterBar from '../components/FilterBar/FilterBar';
+//import FilterBar from '../components/FilterBar/FilterBar';
+import Spinner from '../components/Spinner/Spinner';
+import { Pagination } from 'antd';
+import { Select } from 'antd';
 
-const ItemDetails = {
-  UserAvatar: 'https://semantic-ui.com/images/avatar2/small/mark.png',
-  FullName: 'Elias Afara',
-  Username: 'elias',
-  ItemName: 'Couch',
-  ItemCategory: 'Fourniture',
-  ItemState: 'Used',
-  ItemLocation: 'Tyre, Lebanon',
-  ItemAddress: 'Tyre, Behind Al Ekhlas Resturant',
-  ItemDescription: 'Old and used couch',
-  ItemDateOfCreation: '2020-11-28T00:02:48.503+00:00',
-  ItemID: '123456789',
-  status: true,
-};
+const { Option } = Select;
 
-const {
-  UserAvatar,
-  FullName,
-  Username,
-  ItemName,
-  ItemCategory,
-  ItemState,
-  ItemLocation,
-  ItemAddress,
-  ItemDescription,
-  ItemDateOfCreation,
-  ItemID,
-  status,
-} = ItemDetails;
+const RequestsPage = ({
+  getAllRequests,
+  requests: {
+    allRequests: { requests, totalPages },
+    loading,
+  },
+}) => {
+  const [queries, setQueries] = useState([]);
+  const [queryPage, setQueryPage] = useState('');
+  const [queryStatus, setQueryStatus] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showTimedSpinner, setShowTimedSpinner] = useState(false);
 
-const AvailableRequests = (
-  // Should be an array that contains all the available Requests
-  // maps over the Requests schema
-  <ItemCard
-    UserAvatar={UserAvatar}
-    FullName={FullName}
-    Username={Username}
-    ItemName={ItemName}
-    ItemCategory={ItemCategory}
-    ItemState={ItemState}
-    ItemLocation={ItemLocation}
-    ItemAddress={ItemAddress}
-    ItemDescription={ItemDescription}
-    ItemDateOfCreation={ItemDateOfCreation}
-    ItemID={ItemID}
-    ItemStatus={status}
-  />
-);
+  const timedSpinner = () => {
+    setShowTimedSpinner(true);
+    setTimeout(function () {
+      setShowTimedSpinner(false);
+    }, 1000);
+  };
 
-const CompletedRequests = (
-  // Should be an array that contains all the completed Requests
-  // maps over the Requests schema
-  <>
-    <ItemCard
-      UserAvatar={UserAvatar}
-      FullName={FullName}
-      Username={Username}
-      ItemName={ItemName}
-      ItemCategory={ItemCategory}
-      ItemState={ItemState}
-      ItemLocation={ItemLocation}
-      ItemAddress={ItemAddress}
-      ItemDescription={ItemDescription}
-      ItemDateOfCreation={ItemDateOfCreation}
-      ItemID={ItemID}
-      ItemStatus={false}
-    />
-    <ItemCard
-      UserAvatar={UserAvatar}
-      FullName={FullName}
-      Username={Username}
-      ItemName={ItemName}
-      ItemCategory={ItemCategory}
-      ItemState={ItemState}
-      ItemLocation={ItemLocation}
-      ItemAddress={ItemAddress}
-      ItemDescription={ItemDescription}
-      ItemDateOfCreation={ItemDateOfCreation}
-      ItemID={ItemID}
-      ItemStatus={false}
-    />
-  </>
-);
+  const filterStatus = (value) => {
+    if (value === 'All') {
+      setQueryStatus('');
+      setQueries([]);
+      setQueryPage('page=1');
+    } else {
+      setQueryStatus(`status=${value}`);
+      setQueries([]);
+      setQueryPage('page=1');
+    }
+  };
 
-const RequestsPage = (props) => {
+  const onChange = (page) => {
+    setCurrentPage(page);
+    setQueryPage(`page=${page}`);
+    window.scrollTo(0, 0);
+    timedSpinner();
+    setQueries([]);
+  };
+
+  useEffect(() => {
+    console.log(queries);
+
+    if (queryPage.length > 0) {
+      queries.push(queryPage);
+    }
+    if (queryStatus.length > 0) {
+      queries.push(queryStatus);
+    }
+
+    if (queries.length > 0) {
+      const activeQueries = queries.join('&');
+      getAllRequests(activeQueries);
+    } else {
+      getAllRequests(queryPage);
+    }
+  }, [getAllRequests, queries, queryPage, queryStatus]);
+
+  console.log(requests);
+
+  const FilterBar = (
+    <>
+      <Select
+        defaultValue='All'
+        placeholder='Status'
+        style={{ width: 120 }}
+        onChange={filterStatus}
+      >
+        <Option value='All'>All</Option>
+        <Option value='Available'>Available</Option>
+        <Option value='Completed'>Completed</Option>
+      </Select>
+    </>
+  );
+
   return (
     <div style={{ maxWidth: '1000px', width: 'inherit' }}>
-      <FilterBar />
-      {AvailableRequests}
-      {CompletedRequests}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {showTimedSpinner ? (
+            <Spinner />
+          ) : (
+            <>
+              <div className='filterBar-container'>
+                <div className='filterBar'>
+                  <span>Filter: </span>
+                  &nbsp;&nbsp;&nbsp;
+                  {FilterBar}
+                </div>
+              </div>
+              {requests.map((request) => (
+                <ItemCard
+                  key={request._id}
+                  UserAvatar={request.user.avatar}
+                  FullName={request.user.fullname}
+                  Username={request.user.username}
+                  ItemName={request.name}
+                  ItemCategory={request.category}
+                  ItemStatus={request.status}
+                  ItemDescription={request.description}
+                  ItemDateOfCreation={request.date}
+                  ItemID={request._id}
+                  ItemUserId={request.user.id}
+                  likes={request.likes}
+                  type='request'
+                />
+              ))}
+              <div className='pagination'>
+                <Pagination
+                  defaultCurrent={1}
+                  current={currentPage}
+                  onChange={onChange}
+                  total={totalPages}
+                  showSizeChanger={false}
+                  hideOnSinglePage={true}
+                />
+              </div>
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 };
 
-// Requests.propTypes = {
+RequestsPage.propTypes = {
+  getAllRequests: PropTypes.func.isRequired,
+  requests: PropTypes.object.isRequired,
+};
 
-// }
+const mapStateToProps = (state) => ({
+  requests: state.requests,
+});
 
-export default RequestsPage;
+export default connect(mapStateToProps, { getAllRequests })(RequestsPage);
