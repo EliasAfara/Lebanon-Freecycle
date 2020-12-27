7116c66559200ecea597dac80fd6453ba9c27cfe
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-27 05:23:07 +0200

    ✨ Created FilterBar Component

--- client/src/components/FilterBar/FilterBar.js
+++ client/src/components/FilterBar/FilterBar.js
@@ -1,39 +1,54 @@
 import React from 'react';
 import './FilterBar.css';
+import { Select } from 'antd';
+const { Option } = Select;
 
-const FilterBar = () => {
+const FilterBar = ({ filterStatus, categories }) => {
   return (
     <>
-      {/* <div className='barContainer'>
-      <div>
-        <select defaultValue={'DEFAULT'}>
-          <option value='DEFAULT' disabled hidden>
-            Date
-          </option>
-          <option value='Recent'>Recent</option>
-          <option value='Old'>Old</option>
-        </select>
+      <div className='filterBar-container'>
+        <div className='filterBar'>
+          <div className='filterBar__title'>
+            <span className='filterBar__title-text'>Filter</span>
+          </div>
+          <div className='filterbar__filter'>
+            <label className='filterbar__label'>Status:</label>
+            <Select
+              defaultValue='All'
+              placeholder='Status'
+              style={{ width: 120 }}
+              onChange={filterStatus}
+              title='Select Status'
+            >
+              <Option value='All'>All</Option>
+              <Option value='Available'>Available</Option>
+              <Option value='Completed'>Completed</Option>
+            </Select>
+          </div>
+
+          <div className='filterbar__filter'>
+            <label className='filterbar__label'>Category:</label>
+            <Select
+              showSearch
+              style={{ width: 200 }}
+              placeholder='Select Category'
+              dropdownMatchSelectWidth={false}
+              optionFilterProp='children'
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              {categories.map((category) => {
+                return (
+                  <Option key={category.id} value={category.id}>
+                    {category.title}
+                  </Option>
+                );
+              })}
+            </Select>
+          </div>
+        </div>
       </div>
-      <div>
-        <select defaultValue={'DEFAULT'}>
-          <option value='DEFAULT' disabled hidden>
-            Status
-          </option>
-          <option value='All'>All</option>
-          <option value='Available'>Available</option>
-          <option value='Completed'>Completed</option>
-        </select>
-      </div>
-      <div>
-        <select defaultValue={'DEFAULT'}>
-          <option value='DEFAULT' disabled hidden>
-            Location
-          </option>
-          <option value=''>Tyre</option>
-          <option value=''>Beirut</option>
-        </select>
-      </div>
-    </div> */}
     </>
   );
 };
