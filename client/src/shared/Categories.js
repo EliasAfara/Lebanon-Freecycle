591fb41bb671620363199977c9a8880fc4917c9f
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2020-12-27 05:26:02 +0200

    📝 Created Categories Data

---
+++ client/src/shared/Categories.js
@@ -0,0 +1,101 @@
+export const RequestCategories = [
+  {
+    id: 0,
+    title: 'Jobs',
+  },
+  {
+    id: 1,
+    title: 'Services',
+  },
+  {
+    id: 2,
+    title: 'Social work',
+  },
+  {
+    id: 3,
+    title: 'Books & Educational Supplies',
+  },
+  {
+    id: 4,
+    title: 'Home Furniture & Appliances',
+  },
+  {
+    id: 5,
+    title: 'Home Improvement & Tools',
+  },
+  {
+    id: 6,
+    title: 'Clothes & Accessories',
+  },
+  {
+    id: 7,
+    title: 'Bags & Shoes',
+  },
+  {
+    id: 8,
+    title: 'Toys, Kids & Babies',
+  },
+  {
+    id: 9,
+    title: 'Mobile Phones & Accessories',
+  },
+  {
+    id: 10,
+    title: 'Computer & Other Devices',
+  },
+  {
+    id: 11,
+    title: 'Sports & Equipments',
+  },
+  {
+    id: 12,
+    title: 'Automobiles & Motorcycles',
+  },
+];
+
+export const DonationsCategories = [
+  {
+    id: 1,
+    title: 'Books & Educational Supplies',
+  },
+  {
+    id: 2,
+    title: 'Home Furniture & Appliances',
+  },
+  {
+    id: 3,
+    title: 'Home Improvement & Tools',
+  },
+  {
+    id: 4,
+    title: 'Clothes & Accessories',
+  },
+  {
+    id: 5,
+    title: 'Bags & Shoes',
+  },
+  {
+    id: 6,
+    title: 'Toys, Kids & Babies Stuff',
+  },
+  {
+    id: 7,
+    title: 'Pets & Animals',
+  },
+  {
+    id: 8,
+    title: 'Mobile Phones & Accessories',
+  },
+  {
+    id: 9,
+    title: 'Computer & Other Devices',
+  },
+  {
+    id: 10,
+    title: 'Sports & Equipments',
+  },
+  {
+    id: 11,
+    title: 'Automobiles & Motorcycles',
+  },
+];
