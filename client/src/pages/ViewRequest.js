28f6025132037219cb75c54b9e31859ed14b4d48
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-02 19:49:27 +0200

    ⚡️ Updated with request actions

--- client/src/pages/ViewRequest.js
+++ client/src/pages/ViewRequest.js
@@ -1,39 +1,118 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getSingleRequest } from '../actions/requests';
+import {
+  getSingleRequest,
+  updateRequestStatus,
+  deleteRequest,
+} from '../actions/requests';
 import Spinner from '../components/Spinner/Spinner';
 import SingleItem from '../components/SingleItem/SingleItem';
 
+// Ant Design Delete Model
+import { Modal } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+const { confirm } = Modal;
+
 const ViewRequest = ({
   getSingleRequest,
+  updateRequestStatus,
+  deleteRequest,
   requests: { singleRequests, singleRequestLoading },
+  auth,
   match,
 }) => {
+  const [modalShow, setModalShow] = useState(false);
   const userIDInParam = match.params.id;
+
   useEffect(() => {
     getSingleRequest(userIDInParam);
   }, [getSingleRequest, userIDInParam]);
+
+  const handleComplete = () => {
+    let newStatus = '';
+    if (singleRequests.status === 'Available') {
+      newStatus = 'Completed';
+    } else {
+      newStatus = 'Available';
+    }
+    confirm({
+      title: 'Are you sure?',
+      icon: <ExclamationCircleOutlined />,
+      content: 'Do you really want to update this item status?',
+      okText: 'Update',
+      okType: 'primary',
+      cancelText: 'Cancel',
+      centered: true,
+      onOk() {
+        updateRequestStatus(singleRequests._id, newStatus);
+        console.log('Updated');
+      },
+      onCancel() {
+        console.log('Canceled');
+      },
+    });
+  };
+
+  const handleDelete = () => {
+    confirm({
+      title: 'Are you sure?',
+      icon: <ExclamationCircleOutlined />,
+      content:
+        'Do you really want to delete this post? This process cannot be undone.',
+      okText: 'Delete',
+      okType: 'danger',
+      cancelText: 'Cancel',
+      centered: true,
+      onOk() {
+        deleteRequest(singleRequests._id);
+        console.log('Deleted');
+        return <Redirect to='/requests' />;
+      },
+      onCancel() {
+        console.log('Canceled');
+      },
+    });
+  };
+
   return (
-    <div>
+    <>
       {singleRequestLoading || singleRequests === null ? (
         <Spinner />
       ) : (
         <>
-          <SingleItem item={singleRequests} />
+          <SingleItem
+            item={singleRequests}
+            auth={auth}
+            type='request'
+            handleComplete={handleComplete}
+            handleDelete={handleDelete}
+            modalShow={modalShow}
+            onClickShowModal={() => setModalShow(true)}
+            onClickHideModal={() => setModalShow(false)}
+          />
         </>
       )}
-    </div>
+    </>
   );
 };
 
 ViewRequest.propTypes = {
   getSingleRequest: PropTypes.func.isRequired,
+  updateRequestStatus: PropTypes.func.isRequired,
+  deleteRequest: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
   requests: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  auth: state.auth,
   requests: state.requests,
 });
 
-export default connect(mapStateToProps, { getSingleRequest })(ViewRequest);
+export default connect(mapStateToProps, {
+  getSingleRequest,
+  updateRequestStatus,
+  deleteRequest,
+})(ViewRequest);
