fdd43f6b867f220197e2bfeb50a27e208570af9e
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-03 00:24:08 +0200

    💡 Modified to show carousel controls if arr > 1

--- client/src/components/SingleItem/ImageSlider.js
+++ client/src/components/SingleItem/ImageSlider.js
@@ -5,7 +5,22 @@ import { Carousel, CarouselItem } from 'react-bootstrap';
 const ImageSlider = ({ images, interval, fade }) => {
   return (
     <div className='SliderConatiner'>
-      <Carousel interval={interval} fade={fade}>
+      <Carousel
+        interval={interval}
+        fade={fade}
+        prevIcon={
+          images &&
+          images.length > 1 && (
+            <span aria-hidden='true' className='carousel-control-prev-icon' />
+          )
+        }
+        nextIcon={
+          images &&
+          images.length > 1 && (
+            <span aria-hidden='true' className='carousel-control-next-icon' />
+          )
+        }
+      >
         {images.map((img, index) => {
           return (
             <CarouselItem key={index}>
