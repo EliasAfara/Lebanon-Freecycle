4fb71d3d6ceb923067e9761e5933f4091928cec9
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-02 19:48:22 +0200

    ✨ Bootstrap Model Popup component inspired by IG

---
+++ client/src/components/Modal/ModalPopUp.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import './ModalPopUp.css';
+
+const ModalPopUp = ({ show, onHide, actions }) => {
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={onHide}
+        size='sm'
+        aria-labelledby='actions'
+        centered
+      >
+        <Modal.Body>
+          <div className='actions-popup'>
+            {actions}
+            <button className='action-popup-button' onClick={onHide}>
+              Cancel
+            </button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalPopUp;
