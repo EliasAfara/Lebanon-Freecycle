1730d147bf318a035860395001d59d8e22443544
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-02 19:44:56 +0200

    ⚡️ Improved Design & Added Actions Pop up

--- client/src/components/SingleItem/SingleItem.js
+++ client/src/components/SingleItem/SingleItem.js
@@ -1,68 +1,151 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageSlider from './ImageSlider';
 
 import { formatDate, formatDateMDY } from '../../utils/formatDate';
 import './SingleItem.css';
 import { Link } from 'react-router-dom';
+import { VscEllipsis } from 'react-icons/vsc';
+import ShareIcon from '../SVGComponents/ShareIcon';
+import { BsFillHeartFill, BsHeart } from 'react-icons/bs';
+import { Tooltip, Tag } from 'antd';
+import { CheckCircleOutlined, ClockCircleOutlined } from '@ant-design/icons';
+import ModalPopUp from '../Modal/ModalPopUp';
+import AuthenticatedUserActions from '../Modal/AuthenticatedUserActions';
+import GuestUserActions from '../Modal/GuestUserActions';
 
 const SingleItem = ({
   item: {
     user,
+    _id,
     name,
     category,
     description,
     phoneNumber,
     images,
+    status,
     likes,
     comments,
     date,
     map,
   },
+  auth,
+  type,
+  handleComplete,
+  handleDelete,
+  modalShow,
+  onClickShowModal,
+  onClickHideModal,
 }) => {
+  const [liked, addLike] = useState(false);
+
+  let lebanesePhoneNumber = `+961 ${phoneNumber.slice(3)}`;
   return (
-    <>
-      {images && <ImageSlider images={images} />}
-
-      <h1 className='item-title'>
-        <strong>{name}</strong>
-      </h1>
-
-      <div className='item-header'>
-        <Link to={`/profile/${user.username}`}>
-          <img
-            src={user.avatar}
-            alt='User Avatar'
-            className='user-avatar'
-            loading='lazy'
-            draggable='false'
-          />
-        </Link>
-        <Link to={`/profile/${user.username}`}>
-          <h4 className='user-fullname'>{user.fullname}</h4>
-        </Link>
-        <h4 className='header-date'>
-          <span className='dot-divider'>.</span>
-          <time dateTime={date} title={formatDateMDY(date)}>
-            {formatDate(date)}
-          </time>
-        </h4>
+    <div className='single-item-container'>
+      <div className='single-item-image-slider'>
+        {images && <ImageSlider images={images} interval={3000} fade={true} />}
       </div>
 
+      <div className='item-title-container'>
+        <h1 className='item-title'>
+          <strong>{name}</strong>
+        </h1>
+        <div className='item-title-leftside-content'>
+          {status === 'Available' ? (
+            <Tag icon={<ClockCircleOutlined />} color='processing'>
+              {status}
+            </Tag>
+          ) : (
+            <Tag icon={<CheckCircleOutlined />} color='success'>
+              {status}
+            </Tag>
+          )}
+          <span className='header-love-icon' onClick={() => addLike(!liked)}>
+            {liked ? (
+              <BsFillHeartFill style={{ color: '#f05f70' }} />
+            ) : (
+              <Tooltip title='Love' color={'#f05f70'}>
+                <BsHeart />
+              </Tooltip>
+            )}
+          </span>
+          {likes && likes.length > 0 && (
+            <span className='header-love-score'>{likes.length}</span>
+          )}
+        </div>
+      </div>
+
+      <div className='item-header'>
+        <div className='item-header-info'>
+          <Link to={`/profile/${user.username}`}>
+            <img
+              src={user.avatar}
+              alt='User Avatar'
+              className='user-avatar'
+              loading='lazy'
+              draggable='false'
+            />
+          </Link>
+          <Link to={`/profile/${user.username}`}>
+            <h4 className='user-fullname'>{user.fullname}</h4>
+          </Link>
+          <h4 className='header-date'>
+            <span className='dot-divider'>•</span>
+            <time dateTime={date} title={formatDateMDY(date)}>
+              {formatDate(date)}
+            </time>
+          </h4>
+        </div>
+        <div className='header-icons-container'>
+          <span className='header-icon-span'>
+            <ShareIcon />
+          </span>
+          <span className='header-icon-span' onClick={onClickShowModal}>
+            <VscEllipsis style={{ width: '25px', height: '25px' }} />
+          </span>
+        </div>
+      </div>
+
+      <ModalPopUp
+        show={modalShow}
+        onHide={onClickHideModal}
+        actions={
+          !auth.authLoading && auth.isAuthenticated ? (
+            user.id === auth.user._id ? (
+              <AuthenticatedUserActions
+                itemStatus={status}
+                editLink={`/edit-${type}/${_id}`}
+                onClickHandleComplete={handleComplete}
+                onClickHandleDelete={handleDelete}
+                onHide={onClickHideModal}
+              />
+            ) : (
+              <GuestUserActions />
+            )
+          ) : (
+            <GuestUserActions />
+          )
+        }
+      />
+
       <div className='item-body-container'>
-        <div>
-          <div>
-            <span>Category: </span>
-            <span>{category}</span>
+        <div className='item-body-info'>
+          <div className='item-body-info-line'>
+            <h2 className='item-body-info-title'>Category</h2>
+            <span className='item-body-info-details'>{category}</span>
           </div>
-          <div>
-            <span>Description: </span>
-            <span>{description}</span>
+          <div className='item-body-info-line'>
+            <h2 className='item-body-info-title'>Description</h2>
+            <span className='item-body-info-details'>{description}</span>
           </div>
         </div>
-        <div>
-          <span>Phone Number</span>
-          <span>{phoneNumber}</span>
-        </div>
+        {status === 'Available' && (
+          <div className='item-body-contact'>
+            <h2 className='item-body-info-title'>Phone Number</h2>
+            <span className='item-body-info-details'>
+              {lebanesePhoneNumber}
+            </span>
+          </div>
+        )}
       </div>
 
       {map && (
@@ -79,7 +162,7 @@ const SingleItem = ({
           MAP
         </div>
       )}
-    </>
+    </div>
   );
 };
 
