c08d9b96af49290450265d7bbbc37aea3ab7f62b
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-02 19:46:34 +0200

    ✨ Actions component for non authenticated users

---
+++ client/src/components/Modal/GuestUserActions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ModalPopUp.css';
+
+const GuestUserActions = () => {
+  return (
+    <>
+      <button className='action-popup-button action-popup-button-danger'>
+        Report
+      </button>
+    </>
+  );
+};
+
+export default GuestUserActions;
