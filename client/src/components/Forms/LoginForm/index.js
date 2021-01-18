c487685604de91c067521483f77fab65a8c977a5
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-18 05:04:27 +0200

    📝 Modified Login Form

--- client/src/components/Forms/LoginForm/index.js
+++ client/src/components/Forms/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -6,16 +6,16 @@ import { connect } from 'react-redux';
 import useForm from './useForm';
 import { validateLoginForm } from '../../../utils/validateForm';
 
-import loadable from '@loadable/component';
+import loadable, { lazy } from '@loadable/component';
 
 import { Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
-import * as S from './styles';
+import * as S from '../styles';
 import { loginUser } from '../../../actions/auth';
-import FormContainer from '../../../common/FormContainer';
 
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
+const FormContainer = lazy(() => import('../../../common/FormContainer'));
 const Input = loadable(() => import('../../../common/Input'));
 const Button = loadable(() => import('../../../common/Button'));
 
@@ -48,62 +48,65 @@ const Login = ({ loginUser, isAuthenticated, loginFormLoading }) => {
   }
   return (
     <>
-      <FormContainer maxWidth={'450px'}>
-        <form onSubmit={handleSubmit}>
-          <S.FormTitle>Login to your Account</S.FormTitle>
-          <>
-            <Input
-              type='email'
-              name='email'
-              id='Your Email'
-              placeholder='Your Email Address'
-              value={email}
-              onChange={handleChange}
-              maxWidth
-            />
-            <ValidationType type='email' />
-          </>
-          <>
-            <Input
-              type='password'
-              name='password'
-              id='Password'
-              placeholder='Your Password'
-              value={password}
-              onChange={handleChange}
-              maxWidth
-            />
-            <ValidationType type='password' />
-          </>
-          <S.ButtonContainer>
-            <Button
-              type='submit'
-              value={
-                formLoading ? (
-                  <>
-                    <Spin
-                      indicator={antIcon}
-                      style={{
-                        color: 'inherit',
-                        fontSize: 'inherit',
-                      }}
-                    />{' '}
-                  </>
-                ) : (
-                  <>Submit</>
-                )
-              }
-              disabled={formLoading}
-            />
-          </S.ButtonContainer>
-        </form>
-
-        <S.BottomMessage>
-          <>
-            Don't have an account? <Link to='register'>Sign up here</Link>
-          </>
-        </S.BottomMessage>
-      </FormContainer>
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormContainer maxWidth={'450px'}>
+          <form onSubmit={handleSubmit}>
+            <S.FormTitle>Login to your Account</S.FormTitle>
+            <>
+              <Input
+                type='email'
+                name='email'
+                label='Email'
+                placeholder='Your Email Address'
+                value={email}
+                onChange={handleChange}
+                maxWidth
+                isInvalid={errors.email}
+              />
+              <ValidationType type='email' />
+            </>
+            <>
+              <Input
+                type='password'
+                name='password'
+                label='Password'
+                placeholder='Your Password'
+                value={password}
+                onChange={handleChange}
+                maxWidth
+                isInvalid={errors.password}
+              />
+              <ValidationType type='password' />
+            </>
+            <S.ButtonContainer>
+              <Button
+                type='submit'
+                value={
+                  formLoading ? (
+                    <>
+                      <Spin
+                        indicator={antIcon}
+                        style={{
+                          color: 'inherit',
+                          fontSize: 'inherit',
+                        }}
+                      />{' '}
+                    </>
+                  ) : (
+                    <>Login</>
+                  )
+                }
+                disabled={formLoading}
+              />
+            </S.ButtonContainer>
+          </form>
+          <S.BottomMessage>
+            <>
+              Don't have an account? <Link to='register'>Sign up here</Link>
+            </>
+          </S.BottomMessage>
+        </FormContainer>
+      </Suspense>
     </>
   );
 };
