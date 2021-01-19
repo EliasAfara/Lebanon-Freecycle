82190f911f29ff9adf636031360e3f104eb5bc49
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-19 03:09:36 +0200

    💄 Updated Themes pallate

---
+++ client/src/Themes.js
@@ -0,0 +1,40 @@
+export const lightTheme = {
+  body: '#FAFAFA',
+  headerBackground: '#fff',
+  fontColor: '#363537',
+  activeLinkColor: '#1890ff',
+  toggleBorder: '#dbdbdb',
+  itemBackground: { available: '#fafffa', completed: '#fff' },
+  gradient: 'linear-gradient(#39598A, #79D7ED)',
+  button: {
+    primary: '#1890ff',
+  },
+  formBackground: '#fff',
+};
+
+export const darkTheme = {
+  body: '#222831',
+  headerBackground: '#191C30',
+  fontColor: '#F9FFEE',
+  activeLinkColor: '#985EFF',
+  toggleBorder: '#6B8096',
+  itemBackground: { available: '#191C23', completed: '#191C30' },
+  gradient: 'linear-gradient(#091236, #1E215D)',
+  button: {
+    primary: '#985EFF',
+  },
+  formBackground: '#24191C',
+};
+
+// export const solarizedDarkTheme = {
+//   body: '#FFF',
+//   text: '#363537',
+//   toggleBorder: '#FFF',
+//   background: '#363537',
+// };
+// export const solarizedTheme = {
+//   body: '#363537',
+//   text: '#FAFAFA',
+//   toggleBorder: '#6B8096',
+//   background: '#999',
+// };
