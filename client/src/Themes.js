79dceb00a562b782c51b726b3b3fdee6739ca15c
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-23 00:50:45 +0200

    ✨ Updated Themes Colors

--- client/src/Themes.js
+++ client/src/Themes.js
@@ -5,25 +5,39 @@ export const lightTheme = {
   activeLinkColor: '#1890ff',
   toggleBorder: '#dbdbdb',
   itemBackground: { available: '#fafffa', completed: '#fff' },
-  gradient: 'linear-gradient(#39598A, #79D7ED)',
   button: {
     primary: '#1890ff',
   },
   formBackground: '#fff',
+  inValidInputBackground: '#fffafa !important',
+  modelsBackground: '#fff',
+  models: {
+    background: '#fff',
+    hoverColor: '#f6f6f6',
+  },
+  markerFilter: 'none',
 };
 
+// textcolor: #e6e6e6;
+
 export const darkTheme = {
-  body: '#222831',
-  headerBackground: '#191C30',
+  body: '#18191B',
+  headerBackground: '#242526',
   fontColor: '#F9FFEE',
   activeLinkColor: '#985EFF',
-  toggleBorder: '#6B8096',
-  itemBackground: { available: '#191C23', completed: '#191C30' },
-  gradient: 'linear-gradient(#091236, #1E215D)',
+  toggleBorder: '#3f3f40',
+  itemBackground: { available: '#242526', completed: '#242530' },
   button: {
     primary: '#985EFF',
   },
-  formBackground: '#24191C',
+  formBackground: '#242526',
+  inValidInputBackground: '#442323 !important',
+  modelsBackground: '#383838',
+  models: {
+    background: '#383838',
+    hoverColor: '#252526',
+  },
+  markerFilter: 'hue-rotate(45deg)',
 };
 
 // export const solarizedDarkTheme = {
