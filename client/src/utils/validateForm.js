5a1755cd0499550d76a569e5aedd5ef3c9285640
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-23 09:48:45 +0200

    ⚡️Created Funcs to validate all forms !contact Us

--- client/src/utils/validateForm.js
+++ client/src/utils/validateForm.js
@@ -56,3 +56,119 @@ export const validateRegisterForm = (values) => {
 
   return registerErrors;
 };
+
+export const validateChangePasswordForm = (values) => {
+  let ChangePasswordErrors = {};
+
+  if (!values.oldPassword) {
+    ChangePasswordErrors.oldPassword = 'Old password is required';
+  }
+  if (!values.newPassword) {
+    ChangePasswordErrors.newPassword = 'New password is required';
+  } else if (values.newPassword.length < 6) {
+    ChangePasswordErrors.newPassword =
+      'Please enter a new password with 6 or more characters';
+  }
+
+  if (!values.confirmNewPassword) {
+    ChangePasswordErrors.confirmNewPassword = 'Confirm password is required';
+  } else if (values.newPassword !== values.confirmNewPassword) {
+    ChangePasswordErrors.confirmNewPassword =
+      'Please make sure your new passwords match';
+  }
+
+  return ChangePasswordErrors;
+};
+
+export const validateEditProfileForm = (values) => {
+  let EditProfileErrors = {};
+
+  if (!values.fullname) {
+    EditProfileErrors.fullname = 'Full Name is required';
+  } else if (values.fullname.length < 2 || values.fullname.length > 30) {
+    EditProfileErrors.fullname =
+      'Full Name is required to be between 2-30 characters';
+  }
+
+  if (!values.email) {
+    EditProfileErrors.email = 'Email is required';
+  } else if (!emailRegex.test(values.email)) {
+    EditProfileErrors.email = 'Invalid email address. e.g., email@example.com';
+  }
+
+  if (!values.username) {
+    EditProfileErrors.username = 'Username is required';
+  } else if (values.username.length < 5) {
+    EditProfileErrors.username =
+      'Username is required to be at least 5 characters';
+  } else if (!usernameRegex.test(values.username)) {
+    EditProfileErrors.username =
+      'Username can only use letters, numbers, underscores and periods';
+  }
+
+  if (values.bio.length > 255) {
+    EditProfileErrors.bio = 'Bio is required to be at most 255 characters';
+  }
+
+  return EditProfileErrors;
+};
+
+export const validateDonationsForm = (values) => {
+  let DonationsErrors = {};
+
+  if (!values.name) {
+    DonationsErrors.name = 'Name is required';
+  } else if (values.name.length < 2 || values.name.length > 40) {
+    DonationsErrors.name = 'Name is required to be between 2-40 characters';
+  }
+
+  if (!values.category) {
+    DonationsErrors.category = 'Category is required';
+  }
+  if (!values.locationName) {
+    DonationsErrors.locationName = 'Location is required';
+  }
+  if (!values.address) {
+    DonationsErrors.address = 'Address is required';
+  }
+
+  if (!values.description) {
+    DonationsErrors.description = 'Description is required';
+  } else if (values.description.length < 5 || values.description.length > 255) {
+    DonationsErrors.description =
+      'Description is required to be between 5-255 characters';
+  }
+
+  if (values.phoneNumber.length < 11 || values.phoneNumber.length > 12) {
+    DonationsErrors.phoneNumber = 'Invalid phone number';
+  }
+
+  return DonationsErrors;
+};
+
+export const validateRequestsForm = (values) => {
+  let RequestsErrors = {};
+
+  if (!values.name) {
+    RequestsErrors.name = 'Name is required';
+  } else if (values.name.length < 2 || values.name.length > 40) {
+    RequestsErrors.name = 'Name is required to be between 2-40 characters';
+  }
+
+  if (!values.category) {
+    RequestsErrors.category = 'Category is required';
+  }
+
+  if (!values.description) {
+    RequestsErrors.description = 'Description is required';
+  } else if (values.description.length < 5 || values.description.length > 255) {
+    RequestsErrors.description =
+      'Description is required to be between 5-255 characters';
+  }
+
+  if (values.phoneNumber.length < 11 || values.phoneNumber.length > 12) {
+    RequestsErrors.phoneNumber = 'Invalid phone number';
+  }
+
+  return RequestsErrors;
+};
