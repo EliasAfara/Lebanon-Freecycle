fff7bc8fddf46d775edad10e51418e3a90d1686e
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-01-25 01:09:36 +0200

    ⚡️ Improved Forms Styles

--- client/src/components/Forms/styles.js
+++ client/src/components/Forms/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const FormContainer__Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
 export const FormTitle = styled.div`
   display: flex;
   align-items: center;
@@ -26,4 +33,29 @@ export const ButtonContainer = styled.div`
 export const BottomMessage = styled.div`
   width: 100%;
   margin-top: 10px;
+`;
+
+export const FormField__Div = styled.div`
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+
+  &:first-child {
+    margin-top: 0%;
+  }
+
+  @media (max-width: 767px) {
+    align-items: flex-start;
+    flex-direction: column;
+  }
+`;
+
+export const RequiredMessage__Div = styled.div`
+  width: 140px;
+  margin-right: 86px;
+
+  @media (max-width: 767px) {
+    margin: 10px 0 10px 0;
+    width: 100%;
+  }
 `;
