9224d8852bf83b641e9092d04e76edda84019d13
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-02-05 17:44:41 +0200

    📝 Updated FAQs Page

--- client/src/components/layout/FAQ.js
+++ client/src/components/layout/FAQ.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import Accordion from 'react-bootstrap/Accordion';
+import { Collapse } from 'antd';
+
+const { Panel } = Collapse;
 
 const FAQComponent = ({ content, faqSection }) => {
   return (
@@ -9,20 +10,15 @@ const FAQComponent = ({ content, faqSection }) => {
         <div key={item.id}>
           {item.title === faqSection &&
             item.content.map((contentItem) => (
-              <Accordion key={contentItem.id} style={{ marginBottom: '10px' }}>
-                <Card>
-                  <Accordion.Toggle
-                    as={Card.Header}
-                    eventKey={contentItem.id}
-                    style={{ cursor: 'pointer' }}
-                  >
-                    {contentItem.question}
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey={contentItem.id}>
-                    <Card.Body>{contentItem.answer}</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion>
+              <Collapse
+                key={contentItem.id}
+                expandIconPosition='right'
+                style={{ marginBottom: '8px' }}
+              >
+                <Panel header={contentItem.question} key={contentItem.id}>
+                  <p>{contentItem.answer}</p>
+                </Panel>
+              </Collapse>
             ))}
         </div>
       ))}
@@ -31,16 +27,3 @@ const FAQComponent = ({ content, faqSection }) => {
 };
 
 export default FAQComponent;
-
-// <Accordion expanded={expanded === item.id} onChange={handleChange(item.id)}>
-//   <AccordionSummary
-//     expandIcon={<ExpandMoreIcon style={{ color: '#6C63FF' }} />}
-//     aria-controls='panel1bh-content'
-//     id='panel1bh-header'
-//   >
-//     <Typography className={classes.heading}>{item.question}</Typography>
-//   </AccordionSummary>
-//   <AccordionDetails className={classes.details}>
-//     <Typography>{item.answer}</Typography>
-//   </AccordionDetails>
-// </Accordion>;
--- client/src/pages/FAQPage.js
+++ client/src/pages/FAQPage.js
@@ -2,33 +2,36 @@ import React from 'react';
 import FAQ from '../components/layout/FAQ';
 import { faq } from '../shared/FAQsData';
 import { Card, ListGroup } from 'react-bootstrap';
+import Footer from '../components/layout/Footer';
 import * as S from '../components/layout/styles';
-// import FooterCopyright from '../components/layout/FooterCopyright';
 
 const FAQPage = ({ match }) => {
   const faqSection = match.params.faq;
 
   return (
-    <S.PageContainer_FAQ>
-      <S.SideContentContainer_FAQ>
-        <Card style={{ width: '14rem', height: 'fit-content' }}>
-          <Card.Header>Frequently Asked Questions</Card.Header>
-          <ListGroup variant='flush'>
-            {faq.map((item, index) => (
-              <S.SideBarRoute to={`/faq/${item.title}`} key={index}>
-                <ListGroup.Item>{item.title}</ListGroup.Item>
-              </S.SideBarRoute>
-            ))}
-          </ListGroup>
-        </Card>
-      </S.SideContentContainer_FAQ>
+    <div className='containerMother'>
+      <div className='public-container'>
+        <S.PageContainer_FAQ>
+          <S.SideContentContainer_FAQ>
+            <Card style={{ width: '14rem', height: 'fit-content' }}>
+              <Card.Header>Frequently Asked Questions</Card.Header>
+              <ListGroup variant='flush'>
+                {faq.map((item, index) => (
+                  <S.SideBarRoute to={`/faq/${item.title}`} key={index}>
+                    <ListGroup.Item>{item.title}</ListGroup.Item>
+                  </S.SideBarRoute>
+                ))}
+              </ListGroup>
+            </Card>
+          </S.SideContentContainer_FAQ>
 
-      <S.ContentContainer>
-        <FAQ content={faq} faqSection={faqSection} />
-      </S.ContentContainer>
-
-      {/* <FooterCopyright /> */}
-    </S.PageContainer_FAQ>
+          <S.ContentContainer>
+            <FAQ content={faq} faqSection={faqSection} />
+          </S.ContentContainer>
+        </S.PageContainer_FAQ>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
