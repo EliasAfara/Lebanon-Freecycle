f84324f38d39f649a2fc15302f460e9efc8e1623
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-02-05 17:48:31 +0200

    📝 Updated Footer Component

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,43 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import * as S from './styles';
 
-const Footer = () => {
+const Footer = ({ isAuthenticated }) => {
   return (
-    <S.FooterContainer>
-      <footer>
-        <S.FooterRoutesList>
+    <S.Footer>
+      <S.FooterRoutesList>
+        {!isAuthenticated && (
           <S.FooterRoutesListItem>
             <S.FooterRoutesListItem_Link to='/'>
               Home
             </S.FooterRoutesListItem_Link>
           </S.FooterRoutesListItem>
-          <S.FooterRoutesListItem>
-            <S.FooterRoutesListItem_Link to='/about'>
-              About
-            </S.FooterRoutesListItem_Link>
-          </S.FooterRoutesListItem>
-          <S.FooterRoutesListItem>
-            <S.FooterRoutesListItem_Link to='/contact-us'>
-              Contact us
-            </S.FooterRoutesListItem_Link>
-          </S.FooterRoutesListItem>
-          <S.FooterRoutesListItem>
-            <S.FooterRoutesListItem_Link to='/faq/About'>
-              Help & FAQs
-            </S.FooterRoutesListItem_Link>
-          </S.FooterRoutesListItem>
-          <S.FooterRoutesListItem>
-            <S.FooterRoutesListItem_Link to='#'>
-              Privacy Policy
-            </S.FooterRoutesListItem_Link>
-          </S.FooterRoutesListItem>
-        </S.FooterRoutesList>
-        <S.FooterCopyRight>
-          <>&copy; {new Date().getFullYear()} Copyright: Elias Afara</>
-        </S.FooterCopyRight>
-      </footer>
-    </S.FooterContainer>
+        )}
+
+        <S.FooterRoutesListItem>
+          <S.FooterRoutesListItem_Link to='/about'>
+            About
+          </S.FooterRoutesListItem_Link>
+        </S.FooterRoutesListItem>
+        <S.FooterRoutesListItem>
+          <S.FooterRoutesListItem_Link to='/contact-us'>
+            Contact us
+          </S.FooterRoutesListItem_Link>
+        </S.FooterRoutesListItem>
+        <S.FooterRoutesListItem>
+          <S.FooterRoutesListItem_Link to='/faq/About'>
+            Help & FAQs
+          </S.FooterRoutesListItem_Link>
+        </S.FooterRoutesListItem>
+        <S.FooterRoutesListItem>
+          <S.FooterRoutesListItem_Link to='#'>
+            Privacy Policy
+          </S.FooterRoutesListItem_Link>
+        </S.FooterRoutesListItem>
+      </S.FooterRoutesList>
+      <S.FooterCopyRight>
+        <>&copy; {new Date().getFullYear()} Copyright: Elias Afara</>
+      </S.FooterCopyRight>
+    </S.Footer>
   );
 };
 
-export default Footer;
+Footer.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Footer);
