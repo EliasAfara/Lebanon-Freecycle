8555f092cf834d8fcc9ce3dc9dc64e9c146cb2c8
EliasAfara <39487200+EliasAfara@users.noreply.github.com>
2021-02-08 19:40:17 +0200

    fix

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use('/api/donations', donationsRoute);
 // Serve static assests in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(__dirname, '/client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
